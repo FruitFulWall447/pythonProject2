6623898bc8741c5e02845a08412aadedfe8dd3fb
FruitFulWall447 <kollerkevin1@gmail.com>
2024-05-12 19:14:56 +0300

    def

--- discord_server.py
+++ discord_server.py
@@ -503,15 +503,14 @@ def thread_recv_messages(n, addr):
                 database_func.add_message(sender, receiver, content, type_of_message, file_name)
                 # fix it...
                 if not receiver.startswith("("):
-                    add_message_for_client(receiver, data)
+                    ServerHandler.update_message_for_users([receiver], data)
                 else:
                     # means its a group therefore need to update message for every member of group
                     group_name, group_id = gets_group_attributes_from_format(receiver)
                     group_members = database_func.get_group_members(group_id)
                     print(group_members)
                     group_members.remove(User)
-                    for member in group_members:
-                        add_message_for_client(member, data)
+                    ServerHandler.update_message_for_users([group_members], data)
                 logger.info(f"added new message from {User} to {receiver}")
             elif message_type == "update_profile_pic":
                 b64_encoded_profile_pic = data.get("b64_encoded_profile_pic")
