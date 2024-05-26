7b27305b13d4855c1a40fc1329e3a1514da613d8
FruitFulWall447 <kollerkevin1@gmail.com>
2024-05-26 19:28:03 +0300

    def

--- Discord_app_client.py
+++ Discord_app_client.py
@@ -3077,8 +3077,8 @@ class PageController:
             print(e)
 
     def lost_connection_with_server(self):
-        self.receive_thread_after_login = threading.Thread(target=self.thread_recv_messages, args=())
         self.is_logged_in = False
+        self.receive_thread_after_login = threading.Thread(target=self.thread_recv_messages, args=())
         self.is_waiting_for_2fa_code = False
         self.current_page.close()
         self.server_is_down_page = ServerIsDownPage(self)
